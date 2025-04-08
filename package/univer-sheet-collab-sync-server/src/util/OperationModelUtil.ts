2c624988875280698c465057ab7006c687338116
nahooni0511 <nahooni0511@gmail.com>
2025-04-08 12:16:27 +0900

    Fix protection update delay causing sheet changes to not apply
    - Optimized the protection update mechanism to ensure updates are promptly reflected on the sheet.

--- package/univer-sheet-collab-sync-server/src/util/OperationModelUtil.ts
+++ package/univer-sheet-collab-sync-server/src/util/OperationModelUtil.ts
@@ -1,6 +1,9 @@
 import {IOperationModel} from "@gongback/univer-sheet-collab";
 
 export function isSheetChangeOp(operationModel: IOperationModel): boolean {
+    if (operationModel.id.includes("protection")) {
+        return true;
+    }
     return !!(
         operationModel.cellPayload?.find(a => a.cells.getSizeOf() > 0) ||
         operationModel.rangePayload
